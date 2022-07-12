5be79562ff0bda88f8ab935f1d41db560722fae6
Arvind Varma <av894883@gmail.com>
2022-07-12 12:33:25 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+<div id="acontent">
+<h1 id="me">About <span id="sp6">me</span> </h1>
+<h2 id="dis">Developer <span  id="sp5">&Designer</span></h3>
+<p>I have extensive knowledge of<strong> HTML, JavaScript, SQL, CSS</strong> Combined with my communication and problem-solving skills, I think I would be an excellent candidate for the Web Developer.
+ I am current time join the full stack Web Developer program by <Strong>newthon school</Strong> I design, code, and  Multiple  web project lot of  variety. In my latest project,I have always been passionate about web development, which led me to completed a Bachelor of art degree. I am constantly seeking out new technologies and staying up-to-date on trends in the industry.<br>Thank you for taking the time to review my qualifications. I look forward to sharing more about my skills and experiences. Feel free to contact me at <i>Phone- +918948837171 Or Av894883@gamil.com</i> ||
+<a href="https://www.linkedin.com/in/arvind-varma-884a7423a/" id="Read">Lets-talk</a>
+</div>
